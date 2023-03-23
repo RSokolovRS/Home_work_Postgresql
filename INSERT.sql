169056106fce72839ab04b21c6dfefbaf93b426d
RSokolov <roman-27@inbox.ru>
2023-03-23 14:36:08 +0300

    add_change

--- INSERT.sql
+++ INSERT.sql
@@ -12,18 +12,20 @@ values ('Trustfall', '2020-01-08'), ('iTunes Live from SoHo', '2018-03-16'),
 		('The War on Errorism', '2018-09-06'),('Nevermind', '2018-10-17'), 
 		('Offline', '2022-12-23'), ('A Beautiful Lie', '2019-07-14');
 
-INSERT INTO traks (id_album, name, time_trak) 
-values	(1, 'Long Way to Go', 3.50),(1, 'Kids in Love My', 2.30),
-		(2, 'Fool That I Am', 3.38), (2, 'Melt My Heart to Stone', 3.80), 
-		(3,'Жиган-МойЛимон', 2.94), (3,'Мой Кольщик:)', 4.87),
-		(4, 'Already Home', 4.30), (4, 'Empire State of Mind', 4.41),
-		(5, 'Re-gaining Unconsciousness', 2.39), (5, 'Decom-posuer', 3.59),
-		(6, 'My song', 3.60), (6, 'On a Plain', 3.16),
-		(7, 'Offline', 3.38), (7, 'Close to the my Sun', 3.50),
-	    (8, 'My Beautiful Lie', 4.00), (8, 'From Yesterday', 4.08);
 
+		
+INSERT INTO traks (id_album, name, time_trak) 
+values	('1', 'Long Way to Go', '00:03:50'),('1', 'Kids in Love My', '00:02:30'),
+		('2', 'Fool That I Am', '00:03:38'), ('2', 'Melt My Heart to Stone','00:03:54'), 
+		('3','Жиган-МойЛимон', '00:02:15'), ('3','Мой Кольщик:)', '00:02:40'),
+		('4', 'Already Home', '00:04:30'), ('4', 'Empire State of Mind', '00:04:41'),
+		('5', 'Re-gaining Unconsciousness', '00:02:39'), ('5', 'Decom-posuer', '00:03:27'),
+		('6', 'My song', '00:02:16'), ('6', 'On a Plain', '00:03:18'),
+		('7', 'Offline', '00:03:42'), ('7', 'Close to the my Sun', '00:03:24'),
+	    ('8', 'My Beautiful Lie','00:04:00'), ('8', 'From Yesterday', '00:04:04');
+	   
 INSERT INTO traks (id_album, name, time_trak)
-VALUES (3, 'Фраер', 2.55), (8, 'The Kill', 3.51), (7, 'Like Somebody', 4.54)
+VALUES ('3','Фраер', '00:03:04'), ('6','Come As You Are', '00:03:40');
 	    
 INSERT INTO collection (name, year_of_issur)
 values	('Pop and Hip-Hop', '2022-02-08'), ('Pop-Hits', '2020-12-03'),
@@ -31,6 +33,8 @@ values	('Pop and Hip-Hop', '2022-02-08'), ('Pop-Hits', '2020-12-03'),
 		('Alternative-Hits', '2022-12-19'), ('Шансон', '2018-12-19'),
 		('Зарубежная Эстрада - 80х', '2018-01-01'), ('Rap-Hits', '2020-12-06');
 	
+
+		
 INSERT INTO collection_traks (id_collection, id_trak)
 values	('1', '1'),('1', '2'),('1', '3'),('1', '4'),('1', '5'),('1', '6'),('1', '7'),('1', '8'),
 		('2', '1'),('2', '2'),('2', '3'),('2', '4'),('2', '5'),('2', '6'),
